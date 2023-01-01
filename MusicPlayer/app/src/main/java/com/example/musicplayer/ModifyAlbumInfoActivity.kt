0f9cd83932b219944f5d6da9671404da92b269b6
enteraname74 <noahpenin@gmail.com>
2023-01-01 20:11:25 +0100

    Modifying an album is not applied to the played list

--- MusicPlayer/app/src/main/java/com/example/musicplayer/ModifyAlbumInfoActivity.kt
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/ModifyAlbumInfoActivity.kt
@@ -6,7 +6,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Size
@@ -21,7 +20,7 @@ import kotlinx.coroutines.launch
 
 class ModifyAlbumInfoActivity : Tools() {
     private var albumPos : Int = -1
-    private lateinit var album : Album
+    private lateinit var selectedAlbum : Album
     private lateinit var playlistCoverField : ImageView
     private lateinit var playlistNameField : EditText
 
@@ -30,7 +29,7 @@ class ModifyAlbumInfoActivity : Tools() {
         setContentView(R.layout.activity_modify_playlist_info)
 
         albumPos = intent.getSerializableExtra("POS") as Int
-        album = MyMediaPlayer.allAlbums[albumPos]
+        selectedAlbum = MyMediaPlayer.allAlbums[albumPos]
 
         playlistCoverField = findViewById(R.id.playlist_cover)
         playlistNameField = findViewById(R.id.edit_playlist_name)
@@ -38,9 +37,9 @@ class ModifyAlbumInfoActivity : Tools() {
         findViewById<TextView>(R.id.title_activity).text = getString(R.string.modify_album_informations)
         findViewById<TextView>(R.id.playlist_name).text = getString(R.string.name_of_the_album)
 
-        if (album.albumCover != null){
+        if (selectedAlbum.albumCover != null){
             // Passons d'abord notre byteArray en bitmap :
-            val bytes = album.albumCover
+            val bytes = selectedAlbum.albumCover
             var bitmap: Bitmap? = null
             if ((bytes != null) && bytes.isNotEmpty()) {
                 bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
@@ -50,7 +49,7 @@ class ModifyAlbumInfoActivity : Tools() {
             playlistCoverField.setImageResource(R.drawable.ic_saxophone_svg)
         }
 
-        playlistNameField.setText(album.albumName)
+        playlistNameField.setText(selectedAlbum.albumName)
 
         playlistCoverField.setOnClickListener{ selectImage() }
         val validateButton = findViewById<Button>(R.id.validate_button)
@@ -84,30 +83,46 @@ class ModifyAlbumInfoActivity : Tools() {
         // On modifie les éléments du fichier :
         // Si le nom est déjà prit ou si le nom reste le même, on peut enregistrer les changements
         CoroutineScope(Dispatchers.IO).launch {
-            album.albumName = playlistNameField.text.toString()
+            selectedAlbum.albumName = playlistNameField.text.toString()
             val drawable = playlistCoverField.drawable
             val bitmapDrawable = drawable as BitmapDrawable
             val bitmap = bitmapDrawable.bitmap
 
             val byteArray = bitmapToByteArray(bitmap)
 
-            album.albumCover = byteArray
+            selectedAlbum.albumCover = byteArray
 
-            MyMediaPlayer.allAlbums[albumPos] = album
+            MyMediaPlayer.allAlbums[albumPos] = selectedAlbum
 
             // Mettons à jour toutes les musiques de l'album :
-            for (music in album.albumList) {
-                music.album = album.albumName
-                music.albumCover = album.albumCover
+            for (music in selectedAlbum.albumList) {
+                music.album = selectedAlbum.albumName
+                music.albumCover = selectedAlbum.albumCover
 
                 // Mise à jour dans la playlist principale :
-                MyMediaPlayer.allMusics.find { it.path == music.path }?.album = album.albumName
-                MyMediaPlayer.allMusics.find { it.path == music.path }?.albumCover = album.albumCover
+                MyMediaPlayer.allMusics.find { it.path == music.path }?.apply {
+                    album = selectedAlbum.albumName
+                    albumCover = selectedAlbum.albumCover
+                }
 
                 // Mise à jour dans les palylists :
                 for (playlist in MyMediaPlayer.allPlaylists) {
-                    playlist.musicList.find { it.path == music.path }?.album = album.albumName
-                    playlist.musicList.find { it.path == music.path }?.albumCover = album.albumCover
+                    playlist.musicList.find { it.path == music.path }?.apply {
+                        album = selectedAlbum.albumName
+                        albumCover = selectedAlbum.albumCover
+                    }
+                }
+
+                // Mise à jour des playlists de lecture :
+
+                MyMediaPlayer.initialPlaylist.find { it.path == music.path }?.apply {
+                    album = selectedAlbum.albumName
+                    albumCover = selectedAlbum.albumCover
+                }
+
+                MyMediaPlayer.currentPlaylist.find { it.path == music.path }?.apply {
+                    album = selectedAlbum.albumName
+                    albumCover = selectedAlbum.albumCover
                 }
             }
 
