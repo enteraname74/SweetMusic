b8355c230a520d87e926d6b1f84a1fff54c7e06b
enteraname74 <noahpenin@gmail.com>
2023-01-05 20:03:18 +0100

    Modify the size of the buttons in the bottom panel of music informations

--- MusicPlayer/app/src/main/java/com/example/musicplayer/adapters/Playlists.kt
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/adapters/Playlists.kt
@@ -3,8 +3,6 @@ package com.example.musicplayer.adapters
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
-import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
